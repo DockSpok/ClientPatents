3b94e96439499d448429a17f7d8f1bcb27dc7663
MAURO SIQUEIRA DAMASCENO <43550691+DockSpok@users.noreply.github.com>
2020-09-03 04:26:06 -0300

    Initial commit

---
+++ Controllers/PatentsController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ClientPatents.Models;
+
+namespace ClientPatents.Controllers
+{
+    public class PatentsController : Controller
+    {
+        private readonly string apiUrl = "https://localhost:44383/api/patents";
+
+        public async Task<IActionResult> Index()
+        {
+            List<DtoPatent> listPatents = new List<DtoPatent>();
+
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(apiUrl))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    listPatents = JsonConvert.DeserializeObject<List<DtoPatent>>(apiResponse);
+                }
+            }
+            return View(listPatents);
+        }
+
+        public ViewResult GetPatent() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> GetPatent(int id)
+        {
+            DtoPatent patent = new DtoPatent();
+
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(apiUrl + "/" + id))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    patent = JsonConvert.DeserializeObject<DtoPatent>(apiResponse);
+                }
+            }
+            return View(patent);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdatePatent(int id)
+        {
+            DtoPatent patent = new DtoPatent();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(apiUrl + "/" + id))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    patent = JsonConvert.DeserializeObject<DtoPatent>(apiResponse);
+                }
+            }
+            return View(patent);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateReserva(DtoPatent patent)
+        {
+            DtoPatent patentInput= new DtoPatent();
+            using (var httpClient = new HttpClient())
+            {
+                var content = new MultipartFormDataContent();
+                content.Add(new StringContent(patent.PatentId.ToString()), "PatentId");
+                content.Add(new StringContent(patent.PatentTitle), "PatentTitle");
+                content.Add(new StringContent(patent.PatentNumber), "PatentNumber");
+                content.Add(new StringContent(patent.PatentClaims[0]), "PatentClaims");
+                using (var response = await httpClient.PutAsync(apiUrl, content))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    ViewBag.Result = "Sucesso";
+                    patentInput = JsonConvert.DeserializeObject<DtoPatent>(apiResponse);
+                }
+            }
+            return View(patentInput);
+        }
+    }
+}
---
+++ Models/DtoPatent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClientPatents.Models
+{
+    public class DtoPatent
+    {
+        public int PatentId { get; set; }
+        public string PatentNumber { get; set; }
+        public string PatentTitle { get; set; }
+        public int PatentNumClaims { get; set; }
+        public List<string> PatentClaims { get; set; }
+        public DateTime PatentDate { get; set; }
+    }
+}
